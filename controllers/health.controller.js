375c5496d6bdaa8e0f4968cfd25ac8e3a0b12a48
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-27 17:24:41 +0530

    upadted health check controller

--- controllers/health.controller.js
+++ controllers/health.controller.js
@@ -2,8 +2,30 @@ import { getDBStatus } from '../database/db.js';
 
 export const checkHealth = async (req, res) => {
     // TODO: Implement health check functionality
+    const dbStatus = await getDBStatus();
+    const readyStateText = getReadyStateText(dbStatus.readyState);
+    res.status(200).json({
+        status: "success",
+        data: {
+            dbStatus: {
+                readyState: dbStatus.readyState,
+                readyStateText: readyStateText,
+            },
+        },
+    });
 };
 
 function getReadyStateText(state) {
-    // TODO: Implement get ready state text functionality
+    switch (state) {
+        case 0:
+            return "Disconnected";
+        case 1:
+            return "Connected";
+        case 2:
+            return "Connecting";
+        case 3:
+            return "Disconnecting";
+        default:
+            return "Unknown";
+    }
 }
